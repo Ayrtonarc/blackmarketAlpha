d5251aea5d3a97bb0b6da778e931567eed27714c
Ayrton <ayrton.arc@gmail.com>
2022-03-05 14:59:50 -0800

    5 Registro de cliente y admin

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
 var port = process.env.PORT || 4201;
 
-var cliente_route = require('./routes/cliente')
+var cliente_route = require('./routes/cliente'); //rutas
+var admin_route = require('./routes/admin');
 
 mongoose.connect('mongodb://127.0.0.1:27017/tienda',{useUnifiedTopology: true, useNewUrlParser: true},(err, res)=>{
     if(err){
@@ -31,6 +32,7 @@ app.use((req,res,next)=>{
 });
 
 app.use('/api',cliente_route);
+app.use('/api',admin_route);
 
 
 module.exports = app;
